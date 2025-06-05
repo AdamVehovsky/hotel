5528ae54d6d955082d7b2082da7849eec3fa8889
Adam Vehovský <148778464+AdamVehovsky@users.noreply.github.com>
2025-06-05 23:10:52 +0200

    Update urls.py

--- recepce/urls.py
+++ recepce/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  # Přidána chybějící čárka
+    path("", views.index, name="index"),
     path('export-rezervace/', views.export_rezervace_to_excel, name='export_rezervace'),
 ]
